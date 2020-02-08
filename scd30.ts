2b56d3b5bb25ad72a7634fc66e8f08d4d06f32b9
Ron Smeral <ron.smeral@gmail.com>
2020-02-08 23:49:01 +0100

    Complete SCD30 spec implementation

--- scd30.ts
+++ scd30.ts
@@ -1,13 +1,19 @@
 import {openPromisified} from 'i2c-bus';
 
-import {performCommand, performCommandAndRead} from './util';
+import {performCommand, performCommandAndRead, integerToUint16, booleanToUint16} from './util';
 
 import {
   SCD30_CMD_GET_DATA_READY,
   SCD30_CMD_READ_MEASUREMENT,
   SCD30_CMD_START_PERIODIC_MEASUREMENT,
   SCD30_CMD_STOP_PERIODIC_MEASUREMENT,
-  SCD30_CMD_SET_MEASUREMENT_INTERVAL
+  SCD30_CMD_SET_MEASUREMENT_INTERVAL,
+  SCD30_CMD_AUTO_SELF_CALIBRATION,
+  SCD30_CMD_SET_FORCED_RECALIBRATION,
+  SCD30_CMD_SET_TEMPERATURE_OFFSET,
+  SCD30_CMD_SET_ALTITUDE,
+  SCD30_CMD_READ_FIRMWARE_VERSION,
+  SCD30_CMD_SOFT_RESET
 } from './constants';
 
 const I2C_BUS_NUMBER = 1;
@@ -29,7 +35,7 @@ export const close = async (): Promise<void> => {
 };
 
 /**
- * Used to determine if a measurement can be read from the sensor’s buffer.
+ * Used to determine if a measurement can be read from the sensor's buffer.
  * Whenever there is a measurement available from the internal buffer this function returns true, and false otherwise.
  * As soon as the measurement has been read, the return value changes to false.
  * It is recommended to call this function before calling `readMeasurement`.
@@ -73,17 +79,14 @@ export const readMeasurement = async (): Promise<Measurement> => {
  * compensation (default ambient pressure = 1013.25 mBar). For setting a new ambient pressure when continuous
  * measurement is running, you have to call `startContinuousMeasurement` again.
  *
- * @param pressure Ambient pressure in mBar. Must be a value between 700 and 1400.
+ * @param pressure Ambient pressure in mBar. Must be a value between 700 and 1400, inclusive.
  */
 export const startContinuousMeasurement = async (pressure = 0): Promise<void> => {
   if (pressure < 700 || pressure > 1400) {
     throw new Error('Pressure out of range');
   }
 
-  const pressureUint16 = Buffer.alloc(2);
-  pressureUint16.writeUInt16BE(pressure, 0);
-
-  await performCommand(bus, SCD30_CMD_START_PERIODIC_MEASUREMENT, pressureUint16);
+  await performCommand(bus, SCD30_CMD_START_PERIODIC_MEASUREMENT, integerToUint16(pressure));
 };
 
 /**
@@ -107,15 +110,151 @@ export const getMeasurementInterval = async (): Promise<number> => {
  * Initial value is 2s. The chosen measurement interval is saved in non-volatile memory and thus is not reset
  * to its initial value after power up.
  *
- * @param interval Measurement interval in seconds. Must be a value between 2 and 1800.
+ * @param interval Measurement interval in seconds. Must be a value between 2 and 1800, inclusive.
  */
 export const setMeasurementInterval = async (interval: number): Promise<void> => {
   if (interval < 2 || interval > 1800) {
     throw new Error('Measurement interval out of range');
   }
 
-  const intervalUint16 = Buffer.alloc(2);
-  intervalUint16.writeUInt16BE(interval, 0);
-
-  await performCommand(bus, SCD30_CMD_SET_MEASUREMENT_INTERVAL, intervalUint16);
+  await performCommand(bus, SCD30_CMD_SET_MEASUREMENT_INTERVAL, integerToUint16(interval));
 };
+
+/**
+ * Activates or deactivates automatic self-calibration.
+ *
+ * When activated for the first time, a period of 7 days is needed for the algorithm to find its initial parameters.
+ * The sensor has to be exposed to fresh air for at least 1 hour every day. Also during that period, the sensor may
+ * not be disconnected from power supply, otherwise the procedure is aborted.
+ *
+ * The successfully calculated parameters are stored in non-volatile memory and thus not reset after power up.
+ * The most recently found self-calibration parameters will be actively used for selfcalibration disregarding
+ * the status of this feature. Finding a new parameter set by the here described method will always overwrite the
+ * settings from external recalibration (see chapter 0) and vice-versa. The feature is switched off by default.
+ *
+ * To work properly, SCD30 has to see fresh air on a regular basis. Optimal working conditions are given when the
+ * sensor sees fresh air for one hour every day so that ASC can constantly re-calibrate. ASC only works in continuous
+ * measurement mode. ASC status is saved in non-volatile memory. When the sensor is powered down while ASC is
+ * activated, SCD30 will continue with automatic self-calibration after repowering.
+ *
+ * @param enable Set to true to enable, or to false to disable ASC
+ */
+export const setAutomaticSelfCalibration = async (enable: boolean): Promise<void> =>
+  performCommand(bus, SCD30_CMD_AUTO_SELF_CALIBRATION, booleanToUint16(enable));
+
+/**
+ * Indicates whether automatic self-calibration is active.
+ */
+export const isAutomaticSelfCalibrationActive = async (): Promise<boolean> => {
+  const response = await performCommandAndRead(bus, SCD30_CMD_AUTO_SELF_CALIBRATION, 3);
+
+  const result = response.readUInt16BE(0);
+
+  return result === 1;
+};
+
+/**
+ * Forced recalibration (FRC) is used to compensate for sensor drifts when a reference value of CO2 concentration in
+ * close proximity to the SCD30 is available. For best results, the sensor has to be run in a stable environment in
+ * continuous mode at a measurement rate of 2s for at least two minutes before applying the FRC command and sending
+ * the reference value.
+ *
+ * The FRC supersedes the automatic self-calibration and vice-versa. The FRC method imposes a permanent update of the
+ * CO2 calibration curve which persists after repowering the sensor. The most recently used reference value is retained
+ * in volatile memory and can be read out with `getForcedRecalibrationValue`.
+ *
+ * @param co2ppm Reference CO2 concentration. Value must be between 400 ppm and 2000 ppm, inclusive.
+ */
+export const setForcedRecalibrationValue = async (co2ppm: number): Promise<void> => {
+  if (co2ppm < 400 || co2ppm > 2000) {
+    throw new Error('Reference CO2 concentration out of range');
+  }
+
+  await performCommand(bus, SCD30_CMD_SET_FORCED_RECALIBRATION, integerToUint16(co2ppm));
+};
+
+/**
+ * Returns the most recently used reference value of FRC.
+ * After repowering the sensor, the command will return the standard reference value of 400 ppm.
+ */
+export const getForcedRecalibrationValue = async (): Promise<number> => {
+  const response = await performCommandAndRead(bus, SCD30_CMD_SET_FORCED_RECALIBRATION, 3);
+
+  const result = response.readUInt16BE(0);
+
+  return result;
+};
+
+/**
+ * The on-board RH/T sensor is influenced by thermal self-heating of SCD30 and other electrical components. Design-in
+ * alters the thermal properties of SCD30 such that temperature and humidity offsets may occur when operating the
+ * sensor in end-customer devices. Compensation of those effects is achievable by writing the temperature offset found
+ * in continuous operation of the device into the sensor.
+ *
+ * Temperature offset value is saved in non-volatile memory, and thus will be used for temperature offset compensation
+ * even after repowering.
+ *
+ * @param offset Temperature offset, unit [°C x 100], i.e. one tick corresponds to 0.01°C.
+ */
+export const setTemperatureOffset = async (offset: number): Promise<void> =>
+  performCommand(bus, SCD30_CMD_SET_TEMPERATURE_OFFSET, integerToUint16(offset));
+
+/**
+ * Returns the temperature offset.
+ */
+export const getTemperatureOffset = async (): Promise<number> => {
+  const response = await performCommandAndRead(bus, SCD30_CMD_SET_TEMPERATURE_OFFSET, 3);
+
+  const result = response.readUInt16BE(0);
+
+  return result;
+};
+
+/**
+ * Measurements of CO2 concentration based on the NDIR principle are influenced by altitude. SCD30 can compensate
+ * deviations due to altitude. Setting altitude is disregarded when an ambient pressure was provided in
+ * `startContinuousMeasurement`.
+ *
+ * Altitude value is saved in non-volatile memory, and thus will be used for altitude compensation even after
+ * repowering.
+ *
+ * @param altitude Height over sea level in meters above 0.
+ */
+export const setAltitudeCompensation = async (altitude: number): Promise<void> =>
+  performCommand(bus, SCD30_CMD_SET_ALTITUDE, integerToUint16(altitude));
+
+/**
+ * Returns the altitude compensation value in meters.
+ */
+export const getAltitudeCompensation = async (): Promise<number> => {
+  const response = await performCommandAndRead(bus, SCD30_CMD_SET_ALTITUDE, 3);
+
+  const result = response.readUInt16BE(0);
+
+  return result;
+};
+
+/**
+ * Returns the firmware version of SCD30.
+ */
+export const getFirmwareVersion = async (): Promise<string> => {
+  const response = await performCommandAndRead(bus, SCD30_CMD_READ_FIRMWARE_VERSION, 3);
+
+  const major = response.readUInt8(0);
+  const minor = response.readUInt8(1);
+
+  return `${major}.${minor}`;
+};
+
+/**
+ * Perform soft reset.
+ *
+ * The SCD30 provides a soft reset mechanism that forces the sensor into the same state as after powering up without
+ * the need for removing the power-supply. It does so by restarting its system controller. After soft reset the sensor
+ * will reload all calibrated data. However, it is worth noting that the sensor reloads calibration data prior to every
+ * measurement by default. This includes previously set reference values from ASC or FRC as well as temperature offset
+ * values last setting.
+ *
+ * The sensor is able to receive the command at any time, regardless of its internal state.
+ */
+export const softReset = (): Promise<void> => performCommand(bus, SCD30_CMD_SOFT_RESET);
--- util.ts
+++ util.ts
@@ -1,12 +1,19 @@
-import {crcCheck, crcCompute} from "./crc";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {crcCheck, crcCompute} from './crc';
 
-import {SCD30_ADDRESS} from "./constants";
+import {SCD30_ADDRESS} from './constants';
 
-export const commandToBuffer = (command: number) =>
-  Buffer.from([command >> 8, command & 0xFF])
+export const commandToBuffer = (command: number): Buffer => Buffer.from([command >> 8, command & 0xff]);
 
-export const write = async (bus: any, buf: Buffer): Promise<void> =>
-  bus.i2cWrite(SCD30_ADDRESS, buf.length, buf);
+export const integerToUint16 = (value: number): Buffer => {
+  const buf = Buffer.alloc(2);
+  buf.writeUInt16BE(value, 0);
+  return buf;
+};
+
+export const booleanToUint16 = (value: boolean): Buffer => integerToUint16(value ? 1 : 0);
+
+export const write = async (bus: any, buf: Buffer): Promise<void> => bus.i2cWrite(SCD30_ADDRESS, buf.length, buf);
 
 export const read = async (bus: any, length: number): Promise<Buffer> => {
   const buf = Buffer.alloc(length);
@@ -16,18 +23,15 @@ export const read = async (bus: any, length: number): Promise<Buffer> => {
   const data = crcCheck(buf);
 
   return data;
-}
+};
 
 export const performCommand = async (
   bus: any,
   command: number,
   commandArgs: Buffer = Buffer.from([])
 ): Promise<void> => {
-  await write(bus, Buffer.concat([
-    commandToBuffer(command),
-    crcCompute(commandArgs)
-  ]));  
-}
+  await write(bus, Buffer.concat([commandToBuffer(command), crcCompute(commandArgs)]));
+};
 
 export const performCommandAndRead = async (
   bus: any,
@@ -38,4 +42,4 @@ export const performCommandAndRead = async (
   await performCommand(bus, command, commandArgs);
 
   return read(bus, readLength);
-}
+};
